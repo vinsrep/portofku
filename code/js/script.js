18947bd33d96a7775a434568cd792a03b92feca7
Vincent <59721511+vinsrep@users.noreply.github.com>
2023-12-12 09:08:42 +0700

    Add files via upload

---
+++ code/js/script.js
@@ -0,0 +1,18 @@
+lightGallery(document.getElementById('t1'), {
+    thumbnail: true,
+});
+lightGallery(document.getElementById('t2'), {
+    thumbnail: true,
+});
+lightGallery(document.getElementById('t3'), {
+    thumbnail: true,
+});
+lightGallery(document.getElementById('t4'), {
+    thumbnail: true,
+});
+lightGallery(document.getElementById('t5'), {
+    thumbnail: true,
+});
+lightGallery(document.getElementById('t6'), {
+    thumbnail: true,
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './code/**/*.{html}',
+    './code/*.{html,js}',
+    './index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
